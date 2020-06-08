41b11485b29bfc6de284d76066616ebb8a7f7d4e
Igor Artamonov <igor@artamonov.ru>
2020-06-08 13:29:31 -0400

    solution: show more examples and improved docs

--- examples/rpc/src/main/java/ShowState.java
+++ examples/rpc/src/main/java/ShowState.java
@@ -3,15 +3,19 @@ import io.emeraldpay.polkaj.api.PolkadotApi;
 import io.emeraldpay.polkaj.api.RpcCall;
 import io.emeraldpay.polkaj.apihttp.PolkadotHttpApi;
 import io.emeraldpay.polkaj.json.BlockResponseJson;
+import io.emeraldpay.polkaj.json.RuntimeVersionJson;
+import io.emeraldpay.polkaj.json.SystemHealthJson;
 import io.emeraldpay.polkaj.json.jackson.PolkadotModule;
 import io.emeraldpay.polkaj.types.Hash256;
 
 import java.net.URISyntaxException;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 public class ShowState {
     public static void main(String[] args) throws Exception {
         PolkadotHttpApi client = PolkadotHttpApi.newBuilder().build();
+
         Future<Hash256> hashFuture = client.execute(
                 // use RpcCall.create to define the request
                 // the first parameter is Class / JavaType of the expected result
@@ -19,10 +23,8 @@ public class ShowState {
                 // and optionally a list of parameters for the call
                 RpcCall.create(Hash256.class, "chain_getFinalisedHead")
         );
-        Hash256 hash;
 
-        hash = hashFuture.get();
-        System.out.println("Current head: " + hash);
+        Hash256 hash = hashFuture.get();
 
         Future<BlockResponseJson> blockFuture = client.execute(
                 // Another way to prepare a call, instead of manually constructing RpcCall instances
@@ -32,6 +34,22 @@ public class ShowState {
                 PolkadotApi.commands().getBlock(hash)
         );
         BlockResponseJson block = blockFuture.get();
+
+        String version = client.execute(PolkadotApi.commands().systemVersion())
+                .get(5, TimeUnit.SECONDS);
+
+        RuntimeVersionJson runtimeVersion = client.execute(PolkadotApi.commands().getRuntimeVersion())
+                .get(5, TimeUnit.SECONDS);
+
+        SystemHealthJson health = client.execute(PolkadotApi.commands().systemHealth())
+                .get(5, TimeUnit.SECONDS);
+
+        System.out.println("Software: " + version);
+        System.out.println("Spec: " + runtimeVersion.getSpecName() + "/" + runtimeVersion.getSpecVersion());
+        System.out.println("Impl: " + runtimeVersion.getImplName() + "/" + runtimeVersion.getImplVersion());
+        System.out.println("Peers count: " + health.getPeers());
+        System.out.println("Is syncing: " + health.getSyncing());
+        System.out.println("Current head: " + hash);
         System.out.println("Current height: " + block.getBlock().getHeader().getNumber());
         System.out.println("State hash: " + block.getBlock().getHeader().getStateRoot());
         client.close();
