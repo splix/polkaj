7ee1efcef16b3188fcb7e8b1f020c3c1881e537f
Igor Artamonov <igor@artamonov.ru>
2020-07-17 22:04:54 -0400

    solution: Schnorrkel sign

--- polkaj-schnorrkel/src/main/java/io/emeraldpay/polkaj/schnorrkel/Schnorrkel.java
+++ polkaj-schnorrkel/src/main/java/io/emeraldpay/polkaj/schnorrkel/Schnorrkel.java
@@ -1,11 +1,20 @@
 package io.emeraldpay.polkaj.schnorrkel;
 
+/**
+ * Schnorrkel implements Schnorr signature on Ristretto compressed Ed25519 points, as well as related protocols like
+ * HDKD, MuSig, and a verifiable random function (VRF).
+ *
+ * The Java library is a wrapper around Rust implementation of the algorithms.
+ *
+ * @link <a href="https://github.com/w3f/schnorrkel">Rust implementatiion</a>
+ * @link <a href="https://tools.ietf.org/html/rfc8032">RFC 8032 - Edwards-Curve Digital Signature Algorithm (EdDSA)</a>
+ */
 public class Schnorrkel {
 
-    private static native String sign(byte[] message, byte[] sk);
+    private static native byte[] sign(byte[] pubKey, byte[] secretKey, byte[] message);
 
-    public static String sign(byte[] message, Keypair keypair) {
-        return sign(message, keypair.secretKey);
+    public static byte[] sign(byte[] message, Keypair keypair) throws SchnorrkelException {
+        return sign(keypair.publicKey, keypair.secretKey, message);
     }
 
     static {
--- polkaj-schnorrkel/src/rust/src/lib.rs
+++ polkaj-schnorrkel/src/rust/src/lib.rs
@@ -1,20 +1,52 @@
 extern crate jni;
+extern crate schnorrkel;
+extern crate hex;
+extern crate rand;
 
 use jni::JNIEnv;
 use jni::objects::{JClass};
-use jni::sys::{jbyteArray, jstring};
+use jni::sys::{jbyteArray};
+use schnorrkel::{SecretKey, PublicKey};
+use std::string::String;
+
+const SIGNING_CTX: &'static [u8] = b"substrate";
+
+fn sign(message: Vec<u8>, sk: Vec<u8>, pubkey: Vec<u8>) -> Result<Vec<u8>, String> {
+    let pubkey = PublicKey::from_bytes(pubkey.as_slice())
+        .map_err(|e| e.to_string())?;
+
+    let signature = SecretKey::from_ed25519_bytes(sk.as_slice())
+        .map_err(|e| e.to_string())?
+        .sign_simple(SIGNING_CTX,
+                     message.as_slice(),
+                     &pubkey)
+        .to_bytes()
+        .to_vec();
+    Ok(signature)
+}
 
 #[no_mangle]
 pub extern "system" fn Java_io_emeraldpay_polkaj_schnorrkel_Schnorrkel_sign
-    (env: JNIEnv, _class: JClass, message: jbyteArray, sk: jbyteArray) -> jstring {
+    (env: JNIEnv, _class: JClass, pubkey: jbyteArray, sk: jbyteArray, message: jbyteArray) -> jbyteArray {
 
     let message = env.convert_byte_array(message)
         .expect("Message is not provided");
     let sk = env.convert_byte_array(sk)
         .expect("Secret Key is not provided");
+    let pubkey = env.convert_byte_array(pubkey)
+        .expect("Public Key is not provided");
 
-    let output = env
-        .new_string(format!("Hello, {}!", message.len() + sk.len()))
-        .expect("Couldn't create java string!");
-    output.into_inner()
+    let output = match sign(message, sk, pubkey) {
+        Ok(signature) => {
+            env.byte_array_from_slice(signature.as_slice())
+                .expect("Couldn't create result")
+        },
+        Err(msg) => {
+            let none = env.new_byte_array(0)
+                .expect("Couldn't create empty result");
+            env.throw_new("io/emeraldpay/polkaj/schnorrkel/SchnorrkelException", msg).unwrap();
+            none
+        }
+    };
+    output
 }
