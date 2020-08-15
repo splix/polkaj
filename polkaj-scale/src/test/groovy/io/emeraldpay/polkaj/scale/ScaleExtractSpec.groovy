ebb0d084fb2a786c8b37ab3ad534373e29365cd1
Igor Artamonov <igor@artamonov.ru>
2020-08-15 15:56:08 -0400

    problem: unit test fail

--- polkaj-scale/src/test/groovy/io/emeraldpay/polkaj/scale/ScaleExtractSpec.groovy
+++ polkaj-scale/src/test/groovy/io/emeraldpay/polkaj/scale/ScaleExtractSpec.groovy
@@ -1,18 +1,32 @@
 package io.emeraldpay.polkaj.scale
 
+import io.emeraldpay.polkaj.types.ByteData
 import org.apache.commons.codec.binary.Hex
 import spock.lang.Specification
 
 class ScaleExtractSpec extends Specification {
 
-    def "Extracts from bytes"() {
+    def "Extracts from bytes array"() {
         List<byte[]> source = [
                 "3048656c6c6f20576f726c6421"
         ].collect { Hex.decodeHex(it) }
         when:
         def act = source
                 .stream()
-                .map(ScaleExtract.fromBytes(ScaleCodecReader.STRING))
+                .map(ScaleExtract.fromBytesArray(ScaleCodecReader.STRING))
+                .find()
+        then:
+        act == "Hello World!"
+    }
+
+    def "Extracts from bytes data"() {
+        List<ByteData> source = [
+                "3048656c6c6f20576f726c6421"
+        ].collect { ByteData.from(it) }
+        when:
+        def act = source
+                .stream()
+                .map(ScaleExtract.fromBytesData(ScaleCodecReader.STRING))
                 .find()
         then:
         act == "Hello World!"
@@ -20,7 +34,12 @@ class ScaleExtractSpec extends Specification {
 
     def "Cannot create without reader"() {
         when:
-        ScaleExtract.fromBytes(null)
+        ScaleExtract.fromBytesArray(null)
+        then:
+        thrown(NullPointerException)
+
+        when:
+        ScaleExtract.fromBytesData(null)
         then:
         thrown(NullPointerException)
     }
